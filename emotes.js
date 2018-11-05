3c78a76992c93945d983c60f2e3bf6d65561507f
Jason <github@jallan.co.uk>
2018-11-05 23:34:01 +0000

    Gracefully handle exceptions in emote removal

--- emotes.js
+++ emotes.js
@@ -7,22 +7,35 @@ module.exports = {
 const whitespaceRegex = module.exports.whitespaceRegex = /\s+/g
 const doubleColonRegex = /:(\w|-|\+)+:/g
 const emoteRegexMap = new Map()
+const delim = '[EMOTE-ERROR] '
+const errorMapper = ln => delim + ln
 
 function parseEmotes( emotes, message ) {
   let parsed = message;
 
   for( var emoteID in emotes ) {
-    if( !emotes.hasOwnProperty( emoteID ) ) return
+    let emoteText
+    try {
+      if( !emotes.hasOwnProperty( emoteID ) ) return
 
-    let regex = emoteRegexMap.get( emoteID )
+      let regex = emoteRegexMap.get( emoteID )
 
-    if( !regex ) {
-      const [ start, end ] = emotes[ emoteID ][ 0 ].split( '-' );
-      regex = new RegExp( `\\b${ message.substring( Number( start ), Number( end ) + 1 ) }\\b`, 'g' );
-      emoteRegexMap.set( emoteID, regex )
+      if( !regex ) {
+        const [ start, end ] = emotes[ emoteID ][ 0 ].split( '-' );
+        emoteText = message.substring( Number( start ), Number( end ) + 1 )
+        regex = new RegExp( `\\b${ emoteText }\\b`, 'g' );
+        emoteRegexMap.set( emoteID, regex )
+      }
+
+      parsed = parsed.replace( regex, '' );
+    } catch( e ) {
+      e.message = e.message.split( '/n' ).map( errorMapper ).join( '\n' )
+      console.log(
+        `${ delim }An error occured when parsing for emotes in: ${ emoteID } -> ${ emotes }\n`,
+        `${ delim }Message: ${ message }\n`,
+        e
+      );
     }
-
-    parsed = parsed.replace( regex, '' );
   }
 
   parsed = parsed.replace( doubleColonRegex, '' )
