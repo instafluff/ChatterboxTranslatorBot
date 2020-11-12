e62a1c14513016f4f3a1c9d52d3bcda067f4e884
instafluff <instaflufftv@gmail.com>
2020-11-11 16:48:31 -0800

    Check bad channels

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require( 'dotenv' ).config();
 
 const tmi = require( 'tmi.js' );
 const request = require( 'request' );
+const fetch = require( "node-fetch" );
 const Storage = require( 'node-storage' );
 const ComfyDB = require( "comfydb" );
 
@@ -24,59 +25,76 @@ const serverCount = 1;
 let serverChannels = Object.keys( channels ).concat( botChannelName ).filter( x => randomSimpleHash( x ) % serverCount === serverId );
 console.log( serverChannels );
 
-const client = new tmi.Client({
-  options: { debug: false },
-  connection: {
-	  secure: true,
-	  reconnect: true,
-  },
-  channels: [ botChannelName ].concat( Object.keys( channels ) ),
-  identity: {
-	  username: process.env.TWITCHUSER,
-	  password: process.env.OAUTH
-  },
-} );
-client.on( 'chat', onMessage );
-client.on( 'connected', ( address, port ) => {
-	console.log( `Connected: ${ address }:${ port }` );
-} );
-client.on( 'notice', ( channel, msgid, message ) => {
-	console.log( `Notice: ${ channel } ${ msgid } ${ message }` );
-	switch( msgid ) {
-	case "msg_banned":
-		// Leave this channel
-		console.log( "Leaving banned channel:", channels[ channel ] );
-		delete channels[ channel ];
-		store.put( "channels", channels );
-		break;
+(async () => {
+	// Check and clean up channels
+	for( let i = 0; i < serverChannels.length; i += 100 ) {
+		let chans = serverChannels.slice( i, 100 ).replace( "#", "" );
+		let result = await fetch( `https://api.twitch.tv/helix/user?login=${chans.join( "&login=" )}`, {
+			headers: {
+				"Client-ID": process.env.CLIENT_ID,
+				"Authorization": `Bearer ${process.env.API_AUTH}`
+			}
+		} ).then( r => r.json() );
+		let existing = result.data.map( x => x.login );
+		let badChans = chans.filter( c => !existing.includes( c ) );
+		console.log( "BAD CHANNELS:", badChans );
 	}
-} );
-client.on( 'reconnect', () => console.log( 'Reconnecting' ) );
-client.connect();
-ComfyDB.Connect();
 
-const appInjection = { client, prefixRegex, botChannelName, store, channels, translations, request }
+	const client = new tmi.Client({
+	  options: { debug: false },
+	  connection: {
+		  secure: true,
+		  reconnect: true,
+	  },
+	  channels: [ botChannelName ].concat( Object.keys( channels ) ),
+	  identity: {
+		  username: process.env.TWITCHUSER,
+		  password: process.env.OAUTH
+	  },
+	} );
+	client.on( 'chat', onMessage );
+	client.on( 'connected', ( address, port ) => {
+		console.log( `Connected: ${ address }:${ port }` );
+	} );
+	client.on( 'notice', ( channel, msgid, message ) => {
+		console.log( `Notice: ${ channel } ${ msgid } ${ message }` );
+		switch( msgid ) {
+		case "msg_banned":
+			// Leave this channel
+			console.log( "Leaving banned channel:", channels[ channel ] );
+			delete channels[ channel ];
+			store.put( "channels", channels );
+			break;
+		}
+	} );
+	client.on( 'reconnect', () => console.log( 'Reconnecting' ) );
 
-const errorPrefix = "\n[onMessage]  "
+	client.connect();
+	ComfyDB.Connect();
 
-async function onMessage( channel, userstate, message, self ) {
-  if( self ) return;
-  if( userstate.username === "chattranslator" ) return;
+	const appInjection = { client, prefixRegex, botChannelName, store, channels, translations, request }
 
-  try {
-    if( message.match( prefixRegex ) ) {
-      runCommand( channel, userstate, message, appInjection )
-    } else if( channels[ channel ] ) {
-		// translateMessage( channel, userstate, message, appInjection );
-      await translateMessageWithAzure( channel, userstate, message, appInjection )
-	  // translateMessageComfyTranslations( channel, userstate, message, appInjection );
-    }
-  } catch( error ) {
-    console.log(
-      errorPrefix + "Failed handling message!",
-      errorPrefix + "From:  " + userstate.username,
-      errorPrefix + "Message:  " + message,
-      errorPrefix + "Error:  ", error
-    );
-  }
-}
+	const errorPrefix = "\n[onMessage]  "
+
+	async function onMessage( channel, userstate, message, self ) {
+	  if( self ) return;
+	  if( userstate.username === "chattranslator" ) return;
+
+	  try {
+	    if( message.match( prefixRegex ) ) {
+	      runCommand( channel, userstate, message, appInjection )
+	    } else if( channels[ channel ] ) {
+			// translateMessage( channel, userstate, message, appInjection );
+	      await translateMessageWithAzure( channel, userstate, message, appInjection )
+		  // translateMessageComfyTranslations( channel, userstate, message, appInjection );
+	    }
+	  } catch( error ) {
+	    console.log(
+	      errorPrefix + "Failed handling message!",
+	      errorPrefix + "From:  " + userstate.username,
+	      errorPrefix + "Message:  " + message,
+	      errorPrefix + "Error:  ", error
+	    );
+	  }
+	}
+})();
